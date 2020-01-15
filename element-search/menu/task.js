a5b1627f1d087882b04c592ba83e1c9848cbcd46
Renat <suleymanov_renat@mail.ru>
2020-01-15 22:29:17 +0300

    Задача2. Повышенный уровень сложности.

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,34 @@
+// Задача 2. Повышенный уровень сложности
+// в 26 строчке - вопрос. Просьба пояснить.
+
+
 let menuLink = document.querySelectorAll(".menu__link");
 let arrMenuLink = Array.from(menuLink); // нашли линки
-
-// добавляем вышестоящему элементу с menu_sub класс menu_active при клике на линк
 
 for (i = 0; i < menuLink.length; i++) {
     menuLink[i].onclick = activateMenu;
 }    
 
 function activateMenu(el) {
+    closedMenuActive(); // сначала проверяем есть ли открытое меню и закрываем его
+
     let element = el.target; //берем элемент события
     let ul = element.parentElement; // берем у него ближайший элемент-родитель
-    let menuSub = ul.querySelector("ul"); // смотрим вниз от элемент-родителя в поисках ul
+    let menuSub = ul.querySelector("ul"); // смотрим вниз от элемент-родителя в поисках первого ul
     menuSub.classList.add("menu_active"); // если находим - добавляем menu_active
-    return false; // для главного меню почему-то возвращает ошибку и переходит
-}
+    let menuSubLink = menuSub.querySelectorAll("a"); // берём первый найденный элемент ul и ищем ниже все ссылки
+    for (i = 0; i < menuSubLink.length; i++) { 
+         menuSubLink[i].onclick = function() {
+            return false;  // для каждой ссылки запрещаем переход
+        }
+    }
+    return false; //останавливаем функцию иначе меню открывается и сразу закрывается. 
+}                   //Видимо возвращается в menuLink.onclick для следующего обхода?
     
-
+function closedMenuActive() {
+    let CloseMenuActive = document.querySelectorAll(".menu_active"); // находим все открытые меню
+    for (i = 0; i < CloseMenuActive.length; i++) {
+        CloseMenuActive[i].closest("*").classList.remove("menu_active"); //для каждого найденного класса - находим его ближайшего родителя,                                                       
+    }                                                                       // включая элемент-родитель самого класса и удаляем у него класс menu_active
+}
 
