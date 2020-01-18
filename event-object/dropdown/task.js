aeaff61cb66efe3217e127b8f313d295c11ea831
Renat <suleymanov_renat@mail.ru>
2020-01-18 15:59:47 +0300

    Задача 1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,7 +1,9 @@
+// querySelectorAll применяется из расчёта, что может быть несколько кнопок с меню
+
 
 let dropdownValue = document.querySelectorAll(".dropdown__value");
 for (i = 0; i < dropdownValue.length; i++) {
-dropdownValue[i].onclick = makeActiveList(i);
+dropdownValue[i].onclick = makeActiveList;
 }
 
 let dropdownItem = document.querySelectorAll(".dropdown__item");
@@ -10,20 +12,25 @@ for (i = 0; i < dropdownItem.length; i++) {
 }
 
 
-function makeActiveList(i) {
+function makeActiveList() {
     let dropdownList = document.querySelectorAll(".dropdown__list");
-
-    if (dropdownList[i].classList.contains("dropdown__list_active")) {
-        dropdownList[i].classList.remove("dropdown__list_active");
+    for (i = 0; i < dropdownList.length; i++) {
+        if (dropdownList[i].classList.contains("dropdown__list_active")) {
+            dropdownList[i].classList.remove("dropdown__list_active");
     }   else {
-        dropdownList[i].classList.add("dropdown__list_active");
+            dropdownList[i].classList.add("dropdown__list_active");
     } return false;
+}
 }
 
 function changeValue(eventObj) {
     let element = eventObj.target;
-    let dropdownValue = element.closest(".dropdown__value");
-    dropdownValue.innerHTML = element.innerHTML;
+
+    let dropdown = element.closest(".dropdown"); // находим ближайшего родителя с классом dropdown
+    let dropdownValue = dropdown.querySelector(".dropdown__value"); // идём от него вниз и находим текст основной кнопки
+    dropdownValue.innerHTML = element.innerHTML; // присваиваем ей текст из dropdown__item
+
+    let dropdownList = element.closest(".dropdown__list");
     dropdownList.classList.remove("dropdown__list_active");
     return false;
 }
