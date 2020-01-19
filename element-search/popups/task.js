6eee64b7b8f36df58acf692340a46d28b9a67ff7
Renat <suleymanov_renat@mail.ru>
2020-01-19 11:16:37 +0300

    Исправлено закрытие "Хорошо сделано"

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,27 +4,29 @@
 let modalMain = document.getElementById("modal_main");
 modalMain.classList.add("modal_active");
 
-let CloseClass = document.getElementsByClassName("modal__close");
-let arrayCloseClass = Array.from(CloseClass);
+let closeClasse = document.getElementsByClassName("modal__close");
+
+let success = document.getElementById("modal_success");
 
 // Обработчик для 2 и 3 пункта
-for (i = 0; i < arrayCloseClass.length; i++) {
-arrayCloseClass[i].classList.add("modal_active");
-if (arrayCloseClass[i].classList.contains("show-success")) {
-    arrayCloseClass[i].onclick = returnModalSuccess;
-}   else {
-arrayCloseClass[i].onclick = retutnActive;
-}
+for (i = 0; i < closeClasse.length; i++) {
+    closeClasse[i].classList.add("modal_active");
+    if (closeClasse[i].classList.contains("show-success")) {
+        closeClasse[i].onclick = returnModalSuccess;
+    } else {
+        closeClasse[i].onclick = retutnActive;
+    }
 }
 
 // 2. Сделайте закрытие активного окна по нажатию на его элемент с классом modal__close
 function retutnActive() {
     modalMain.classList.remove("modal_active");
+    success.classList.remove("modal_active");
+
 }
 
 // 3. По нажатию на элемент с классом show-success покажите окно #modal_success
 function returnModalSuccess() {
-    let success = document.getElementById("modal_success");
     modalMain.classList.remove("modal_active");
     success.classList.add("modal_active");
 }
